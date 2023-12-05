dac28fb3b53b849fbe3d70c2498b54986cd3fe02
Maxim <serebryakov.maxim@gmail.com>
2023-12-05 08:26:25 +0300

    delete cycle

--- tpa/admin.py
+++ tpa/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
-from .models import Machine, Cycle, Event, Job
+from .models import Machine, Event, Job
 
 admin.site.register(Machine)
-admin.site.register(Cycle)
+#admin.site.register(Cycle)
 admin.site.register(Event)
 admin.site.register(Job)
