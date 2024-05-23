5536060c51769dcb7400f59d666b8dc44d3f8276
Maxim <serebryakov.maxim@gmail.com>
2024-05-23 13:33:06 +0300

    корректировка

--- tpa/views.py
+++ tpa/views.py
@@ -348,7 +348,7 @@ class EffectCycleApiView(APIView):
 
                 socketplan = job_ob.socket_plan
                 socketfact = job_ob.socket_fact 
-                timeplan_ms = job_ob.time_ms
+                timeplan_ms = job_ob.time_plan_ms
 
             except Job.DoesNotExist:
                 pass
