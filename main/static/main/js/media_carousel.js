b0d5d2103a1ddfe829358a76fb733ae11ad05997
Maxim <serebryakov.maxim@gmail.com>
2024-12-12 10:38:30 +0300

    Добавил переход через 60 сек на страницу медиа

---
+++ main/static/main/js/media_carousel.js
@@ -0,0 +1,8 @@
+var mytime = mytime_const = 60;
+document.onmousemove = document.onkeydown = document.onscroll = function(){mytime = mytime_const};
+
+setInterval(function(){
+    //console.log(mytime);
+    mytime --;
+    if (mytime<=0)location.href = window.location.protocol + "//" + window.location.host + "/pressforms/media/?" + location.href;
+}, 1000);
