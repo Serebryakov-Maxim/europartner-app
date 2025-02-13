59ab9884a9391f29120469e8bbc4250af97450e0
Maxim <serebryakov.maxim@gmail.com>
2025-02-13 10:31:30 +0300

    Исправление

--- sensors/views.py
+++ sensors/views.py
@@ -174,7 +174,7 @@ class LastParametersApiView(APIView):
         if len(filter):
             value = 0
             date = ''
-            data = ValueParameter.objects.filter(**filter).order_by('date')[:1]
+            data = ValueParameter.objects.filter(**filter).order_by('-date')[:1]
             if len(data) > 0:
                 value = data[0].value
                 date = data[0].date
