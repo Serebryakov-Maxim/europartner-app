0225f7b4766f60a0edb3cd75efc714456b31d1d3
Maxim <serebryakov.maxim@gmail.com>
2026-07-16 11:29:55 +0300

    err2

--- europartner/cron.py
+++ europartner/cron.py
@@ -36,7 +36,8 @@ def checking_controllers():
     message = 'Последние показания были ' + str(int(total_seconds)) + ' сек. назад'
     text = '<b>' + subject + '</b>' + '\n' + '<p>' + message + '</p>'
 
-    bot_ERP.send_message(settings.MAX_USER_ID_SEREBRYAKOV, text)
+    bot = bot_ERP()
+    bot.send_message(settings.MAX_USER_ID_SEREBRYAKOV, text)
 
     #print("Рассылка на почту")
     #send_mail(subject, message, settings.EMAIL_USER_SENDER, settings.EMAIL_USER_ALERT)
