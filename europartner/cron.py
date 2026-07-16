d75a111991b3cf1b53e6277149cfd2e781b4f23f
Maxim <serebryakov.maxim@gmail.com>
2026-07-16 11:26:20 +0300

    err1

--- europartner/cron.py
+++ europartner/cron.py
@@ -4,7 +4,7 @@ from datetime import datetime
 from tpa.models import Cycle
 from django.utils import timezone
 from django.conf import settings
-from bot_MAX import bot_ERP
+from europartner.bot_MAX import bot_ERP
 import subprocess
 
 def checking_controllers():
@@ -35,9 +35,8 @@ def checking_controllers():
     subject = 'Нет данных с контроллеров ТПА!'
     message = 'Последние показания были ' + str(int(total_seconds)) + ' сек. назад'
     text = '<b>' + subject + '</b>' + '\n' + '<p>' + message + '</p>'
-    
-    bot = bot_ERP()
-    bot.send_message(settings.MAX_USER_ID_SEREBRYAKOV, text)
+
+    bot_ERP.send_message(settings.MAX_USER_ID_SEREBRYAKOV, text)
 
     #print("Рассылка на почту")
     #send_mail(subject, message, settings.EMAIL_USER_SENDER, settings.EMAIL_USER_ALERT)
